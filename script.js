2f8a404d7dc46418154be98b022340b5cd3e9176
UZD007 <djdh9515@gmail.com>
2025-04-12 17:39:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let ism=prompt("Ism kiriting:");
+let familiya=prompt("Familiya kiriting");
+let yosh=prompt("Yoshingizni kiriting");
+let fan=prompt("Yoqtirgan faningizni yozing");
+let tuman=prompt("Yashaydigan tumaningizni kiriting");
+
+
+console.log("Szining ismingiz: " +ism);
+console.log("Sizning familiyangiz: " +familiya);
+console.log("Sizning yoshingiz: " +yosh);
+console.log("Sizning yoqtirgan faningiz: " +fan);
+console.log("Sizning yashaydigan tumaningiz: " +tuman);
+
+alert("Ma'lumotlaringizni kiritganingiz uchun tashakkur")
